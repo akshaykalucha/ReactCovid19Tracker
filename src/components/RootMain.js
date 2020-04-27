a5290f218ad2ce1c6727befde43fcad84a923c95
akshay kalucha <akshaykalucha@gmail.com>
2020-04-27 17:47:35 +0530

    cssStyling

--- src/components/RootMain.js
+++ src/components/RootMain.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import App from './FunctionComps/App'
+// import App from './FunctionComps/App'
+import Dynamicstyle from '../components/dynamicstyle/Dynamicstyle'
 
 class RootMain extends Component {
   render() {
     return (
-      <App />
+      <Dynamicstyle />
     );
   }
 }
