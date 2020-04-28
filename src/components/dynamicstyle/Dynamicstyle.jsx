b9b4c6e889ba03802ce9ce3d9b52ab14b3df671d
akshay kalucha <akshaykalucha@gmail.com>
2020-04-28 23:50:29 +0530

    HooksCSS

--- src/components/dynamicstyle/Dynamicstyle.jsx
+++ src/components/dynamicstyle/Dynamicstyle.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Card from './Card'
-import styles from './Card.module.css'
+
 
 
 export default function dynamicstyle() {
     return (
         <div>
-            <Card />
         </div>
     )
 }
